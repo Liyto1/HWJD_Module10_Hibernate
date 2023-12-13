7e75afdf3a4a8202970d1aa3b5bd988de1f59159
Lev <lev.lvi2@gmail.com>
2023-12-13 16:56:13 +0200

    update 13.12

--- src/main/java/org/example/services/PlanetDao.java
+++ src/main/java/org/example/services/PlanetDao.java
@@ -1,4 +1,19 @@
 package org.example.services;
 
+import org.example.entities.Planet;
+
+import java.util.List;
+
 public interface PlanetDao {
+    boolean createPlanet(Planet planet);
+
+    boolean updatePlanet(Planet planet);
+
+    Planet getPlanetById(String planetId);
+
+    List<Planet> getAllPlanets();
+
+    void deletePlanetById(String planetId);
+
+    void deletePlanet(Planet planet);
 }
